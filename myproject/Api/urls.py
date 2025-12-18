695f1fe0eb396b4744dba10d644e06722cc6360e
Богдан Саух <BohdanSaukh@github.com>
2025-12-18 22:16:31 +0200

    k

--- myproject/Api/urls.py
+++ myproject/Api/urls.py
@@ -1,12 +1,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import AnalyticsViewSet
 
 router = DefaultRouter()
 router.register(r'authors', views.AuthorViewSet, basename='author')
 router.register(r'books', views.BookViewSet, basename='book')
 router.register(r'publishers', views.PublisherViewSet, basename='publisher')
 router.register(r'orders', views.BookOrderViewSet, basename='order')
+router.register(r'analytics', AnalyticsViewSet, basename='analytics') 
 
 urlpatterns = [
     path('', include(router.urls)),
