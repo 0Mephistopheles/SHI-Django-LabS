f765fb15093f1553bbce2bd9f2555e0c5be36da1
0Mephistopheles <pavlo.yarovoi.shi.2024@lpnu.ua>
2025-12-18 20:52:06 +0200

    progress

---
+++ myproject/Services/generator/warehouse/warehouse_data.py
@@ -0,0 +1,27 @@
+# services/generator/warehouse_data.py
+
+LOCATIONS = [
+    "Kyiv - Central",
+    "Lviv - Industrial Zone",
+    "Odesa Port",
+    "Dnipro Logistics Park",
+    "Kharkiv East",
+]
+
+FIRST_NAMES = [
+    "Ivan",
+    "Olena",
+    "Andrii",
+    "Nazar",
+    "Iryna",
+]
+
+LAST_NAMES = [
+    "Petrenko",
+    "Shevchenko",
+    "Kovalenko",
+    "Melnyk",
+    "Bondarenko",
+]
+
+CAPACITY_RANGE = (50, 10000)
